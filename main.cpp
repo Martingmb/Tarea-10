70c0f503815d9bb83d5d9d1c19d1b31be3426314
Mickey1412 <marochac96@gmail.com>
2016-11-19 18:55:00 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,43 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+ int iCant, iNumero, iMatriz[50][50], iFila[50], iVec[50];
+    //Variables para leer el archivo
+    ifstream inArchivo;
+    string sArchivo;
+    
+    //Se pregunta por el nombre del archivo
+    cout << "Ingrese el nombre del archivo, no olvide poner '.txt' (nombre.txt) " << endl;
+    cin >> sArchivo;
+    
+    //Se abre el archivo para leer y obtener los datos
+    inArchivo.open(sArchivo.c_str()); //Checar el directorio
+    
+    inArchivo >> iCant;
+    inArchivo.ignore();
+    
+    while(inArchivo)
+    {
+        for(int iI = 0; iI < iCant; iI++)
+        {
+            for(int iJ = 0; iJ < iCant; iJ++)
+            {
+                inArchivo >> iMatriz[iI][iJ];
+            }
+        }
+    }
+    //Se cierra el archivo
+    inArchivo.close();
+    
+    //Desplegar valores
+    cout << iCant << endl;
+    for(int iI = 0; iI < iCant; iI++)
+    {
+        for(int iJ = 0; iJ < iCant; iJ++)
+        {
+            cout << iMatriz[iI][iJ];
+        }
+        cout << endl;
+    }
+
 }
